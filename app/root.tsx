5b9646d2a75eeddbd15b78cfdcb305dd86eb9455
Andrew Byrd <andrew.byrd99@gmail.com>
2024-04-04 20:40:23 -0400

    Google ads

--- app/root.tsx
+++ app/root.tsx
@@ -17,6 +17,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6846086051700160"
+          crossOrigin="anonymous"
+        />
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
