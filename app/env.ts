32947dadff7d7c29295ce5fe9964abc0c2d553a0
Andrew Byrd <andrew.byrd99@gmail.com>
2024-05-09 00:02:03 -0400

    Fixing up the redis creation

--- app/env.ts
+++ app/env.ts
@@ -6,6 +6,8 @@ export const env = createEnv({
   server: {
     ANTHROPIC_API_KEY: z.string().min(1),
     UPSTASH_REDIS_URL: z.string().min(1),
+    UPSTASH_REDIS_PORT: z.coerce.number(),
+    UPSTASH_REDIS_PASSWORD: z.string().min(1),
     TURSO_DB_URL: z.string().min(1),
     TURSO_DB_TOKEN: z.optional(z.string().min(1)),
     GENERIC_SESSION_SECRET: z.string().min(1),
--- app/utils/helpers/server/rate-limiter.server.ts
+++ app/utils/helpers/server/rate-limiter.server.ts
@@ -2,7 +2,11 @@ import { env } from "~/env";
 import Redis from "ioredis";
 import { RateLimiterRedis } from "rate-limiter-flexible";
 
-const client = new Redis(env.UPSTASH_REDIS_URL, {
+const client = new Redis({
+  host: env.UPSTASH_REDIS_URL,
+  port: env.UPSTASH_REDIS_PORT,
+  username: "default",
+  password: env.UPSTASH_REDIS_PASSWORD,
   family: 6,
 });
 
